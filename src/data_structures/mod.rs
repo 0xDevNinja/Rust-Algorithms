15460feef3abe88bc03a0ae3eca967d0c008d23f
0xDevNinja <manmit0x@gmail.com>
2026-04-30 00:55:03 +0530

    style: cargo fmt

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -34,12 +34,12 @@ pub mod interval_tree;
 
 pub mod fibonacci_heap;
 
-pub mod segment_tree_2d;
 pub mod b_tree;
 pub mod dynamic_segment_tree;
 pub mod kd_tree_2d;
 pub mod merge_sort_tree;
 pub mod red_black_tree;
+pub mod segment_tree_2d;
 pub mod splay_tree;
 pub mod treap;
 
