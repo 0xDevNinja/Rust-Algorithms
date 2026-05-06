f54fca84b3f2c1da2629676108fe24b501a6bc06
0xDevNinja <manmit0x@gmail.com>
2026-05-07 00:36:01 +0530

    feat(geometry): add Sutherland-Hodgman polygon clipping
    Closes #126

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -9,6 +9,7 @@ pub mod polygon_centroid;
 pub mod rectangle_union_area;
 pub mod rotating_calipers;
 pub mod segment_intersection;
+pub mod sutherland_hodgman;
 pub mod welzl_smallest_enclosing_circle;
 
 pub mod manhattan_chebyshev;
