0778939a970d2bef77d4c728a0f8dc5b1f597cfb
0xDevNinja <manmit0x@gmail.com>
2026-05-09 10:30:00 +0000

    feat(greedy): add first-fit-decreasing bin packing

--- src/greedy/mod.rs
+++ src/greedy/mod.rs
@@ -1,6 +1,7 @@
 //! Greedy choice algorithm recurrences.
 
 pub mod activity_selection;
+pub mod bin_packing_ffd;
 pub mod boyer_moore_majority_vote;
 pub mod fractional_knapsack;
 pub mod job_sequencing;
