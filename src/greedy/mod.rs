7900d9d5ea76997a388d3cafcce45dff145e84d9
0xDevNinja <manmit0x@gmail.com>
2026-05-09 09:00:00 +0000

    feat(greedy): add Huffman coding

--- src/greedy/mod.rs
+++ src/greedy/mod.rs
@@ -4,6 +4,7 @@ pub mod activity_selection;
 pub mod bin_packing_ffd;
 pub mod boyer_moore_majority_vote;
 pub mod fractional_knapsack;
+pub mod huffman;
 pub mod job_sequencing;
 pub mod set_cover;
 pub mod stable_matching;
