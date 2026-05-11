34b3cc800ce644a5b0e5f4c9286283965db3ff52
0xDevNinja <manmit0x@gmail.com>
2026-05-11 09:30:00 +0000

    feat(dp): add winning-strategy DP for two-player games on a DAG

--- src/dynamic_programming/mod.rs
+++ src/dynamic_programming/mod.rs
@@ -41,6 +41,7 @@ pub mod divide_and_conquer_optimization;
 pub mod buy_sell_stock;
 pub mod convex_hull_trick;
 pub mod digit_dp;
+pub mod game_dag;
 pub mod held_karp;
 pub mod max_product_subarray;
 pub mod minimax_alpha_beta;
